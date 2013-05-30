49479da31fa8ed068180db100d4e92fe17e16fe0
Erik Weitnauer <eweitnauer@gmail.com>
2013-05-30 10:40:35 +0200

    fixed bug

--- src/settings.js
+++ src/settings.js
@@ -1,29 +1,31 @@
-var pbpSettings = {
-/* TODO: use this everywhere */ max_dist: 0.06 // maximal distance of an objects to a spatial group to belong to it
-/* TODO: use this everywhere */,activation_threshold: 0.5
- ,attrs: {
- 	  //'count': CountAttribute // TODO: this is a group attribute and must be treated differently
-    'left_pos': LeftAttribute
-	 ,'right_pos': RightAttribute
-	 ,'bottom_pos': BottomAttribute
-	 ,'top_pos': TopAttribute
-	 ,'top_most': TopMostAttribute
-	 ,'on_ground': OnGroundAttribute
-	 ,'shape': ShapeAttribute
-	 ,'stability': StabilityAttribute
-	 ,'small': SmallAttribute
-	 ,'large': LargeAttribute
-	 ,'moves': MovesAttribute
- }
- ,rels: {
-	  'above': AboveRelationship
-	 ,'below': BelowRelationship
-	 ,'left': LeftRelationship
-	 ,'right': RightRelationship
-	 ,'beside': BesideRelationship
-	 ,'far': FarRelationship
-	 ,'close': CloseRelationship
-	 ,'ontop': OnTopRelationship
-	 ,'touch': TouchRelationship
- }
-}
+var pbpSettings = (function() {
+	res = {
+    max_dist: 0.06 // maximal distance of an objects to a spatial group to belong to it /* TODO: use this everywhere */
+   ,activation_threshold: 0.5 /* TODO: use this everywhere */
+   ,attrs: {}
+   ,rels: {}
+	};
+	// attributes
+	[LeftAttribute,
+	 RightAttribute,
+	 BottomAttribute,
+	 TopAttribute,
+	 TopMostAttribute,
+	 OnGroundAttribute,
+	 ShapeAttribute,
+	 StabilityAttribute,
+	 SmallAttribute,
+	 LargeAttribute,
+	 MovesAttribute].forEach(function (attr) { res.attrs[attr.prototype.key] = attr });
+	// relations
+	[AboveRelationship,
+	BelowRelationship,
+	LeftRelationship,
+	RightRelationship,
+	BesideRelationship,
+	FarRelationship,
+	CloseRelationship,
+	OnTopRelationship,
+	TouchRelationship].forEach(function (rel) { res.rels[rel.prototype.key] = rel });
+	return res;
+})();
