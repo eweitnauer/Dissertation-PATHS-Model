184cfa69458a0d02c33eb67f976ec6289588a46a
Erik Weitnauer <eweitnauer@gmail.com>
2013-06-12 14:43:49 +0200

    complete refactoring of selector and solution

--- src/interpreter/selector.js
+++ src/interpreter/selector.js
@@ -1,43 +1,197 @@
 // Copyright 2013, Erik Weitnauer.
 
-/// A selector is used to focus on a subset of objects in a scene.
-var Selector = function() {
-	this.attrs = {};
+/// The Selector is used to focus on a subset of objects in a scene based on
+/// a number of attributes and relations that must be fullfilled. The selector
+/// can be in one of 4 modes:
+/// - all ... selects all matching nodes
+/// - first ... selects first of the matching nodes
+/// - unique ... only selects a node if its the only matching one
+/// - group ... all passed nodes are first put into a group
+///
+/// The select method returns an array of ObjectNodes or GroupNodes according to
+/// the mode. The array is empty if nothing matched.
+var Selector = function(mode) {
+	this.mode = mode || 'all';
+	this.attrs = [];
+	this.rels = [];
 }
 
-/// Will extract the attributes name, activation and label. Pass the time
+/// Returns true if the selector matches anything
+Selector.prototype.empty = function() {
+	return this.attrs.length == 0 && this.rels.length == 0;
+}
+
+/// Will extract the attribute's key, label, activation and constant property. Pass the time
 /// at which the attribute values should match (default: 'start').
-Selector.prototype.add_attr = function(attr, time) {
-	this.attrs[attr.key] = { label: attr.get_label()
-	                         ,active: attr.get_activity() >= pbpSettings.activation_threshold
-	                         ,time: time || 'start'
-	                         ,constant: attr.constant };
+Selector.prototype.use_attr = function(attr, time) {
+	this.attrs.push(Selector.AttrMatcher.fromAttribute(attr, time));
 };
 
-/// Returns true if the passed object node matches the selector.
-/// If the object does not have one of the selectors attributes set, it does not match.
-Selector.prototype.matches = function(on) {
-	return d3.entries(this.attrs).every(function(e){
-		var attr = e.value;
-		var obj_attr = on.times[attr.time][e.key];
-		if (!obj_attr) return false;
-		var obj_active = obj_attr.get_activity() >= pbpSettings.activation_threshold;
-		return (obj_active == attr.active && obj_attr.get_label() == attr.label);
-	});
+/// Adds the passed AttrMatcher.
+Selector.prototype.add_attr = function(attr_matcher) {
+	this.attrs.push(attr_matcher);
 };
 
-/// Returns an array with all objects in the scene that match the selector.
-Selector.prototype.select = function(scene_node) {
-	return scene_node.objs.filter(this.matches.bind(this));
+/// Will extract the relation key, label, activation, constant and symmetry properties. Pass the time
+/// at which the attribute values should match (default: 'start'). Pass a selector that selects the other
+/// object.
+Selector.prototype.use_rel = function(other, rel, time) {
+	this.rels.push(Selector.RelMatcher(other, rel, time));
 };
 
-/// Returns a human readable description of the selector. If singular is passed
-/// as true, the statment is done in sigular.
-Selector.prototype.describe = function(singular) {
-	var res = d3.values(this.attrs).map(function(attr) {
-		return attr.label + (attr.constant ? '' : ' at the ' + attr.time);
-	});
-	var last = res.pop();
-	if (res.length > 0)	return res.join(', ') + ' and ' + last;
-	else return last;
+/// Adds the passed RelMatcher.
+Selector.prototype.add_rel = function(rel_matcher) {
+	this.rels.push(rel_matcher);
 };
+
+/// Returns true if the passed object node matches the selectors attributes and relations.
+/// Optionally, an array of nodes that will be condisered as relationship partners can be
+/// passed as second parameter. If it isn't, all objects in the scene except `on` are used.
+/// If the selector has no attributes or relationships, every object matches.
+/// If a test_fn is passed, it is called for each node that matches the selector
+/// attributes and only if the function returns true, the node is used. The relationships
+/// of the selector are not used in this case.
+Selector.prototype.matches = function(on, others, test_fn) {
+	return this.attrs.every(function (attr) { return attr.matches(on) }) &&
+				 (test_fn ? test_fn(on)
+				 	        : this.rels.every(function (rel) { return rel.matches(on, others) }));
+};
+
+/// Returns an array with the matching object or group nodes, depending on the mode.
+/// - all ... selects all matching nodes
+/// - one ... selects one of the matching nodes (the first one found)
+/// - unique ... only selects a node if its the only matching one
+/// - group ... puts all nodes in a group and returns it if it matches
+/// If a test_fn is passed, it is called for each node that matches the selector
+/// attributes and only if the function returns true, the node is used. The relationships
+/// of the selector are not used in this case.
+Selector.prototype.select = function(nodes, scene_node, test_fn) {
+	var res = [];
+	if (this.mode == 'group') {
+		group_node = new GroupNode(scene_node, nodes.map(function (on) { return on.obj }));
+		nodes = [group_node];
+	}
+	for (var i=0; i<nodes.length; i++) {
+		var node = nodes[i];
+		if (!this.matches(node, null, test_fn)) continue;
+		else if (this.mode == 'first') return [node];
+		else res.push(node);
+	}
+	if (this.mode == 'unique') return res.length == 1 ? res : [];
+	else return res;
+};
+
+/// Returns a human readable description of the attributes used in this selector.
+Selector.prototype.describe = function() {
+	if (this.empty()) {
+		if (this.mode == 'all') return 'all objects';
+		if (this.mode == 'unique') return 'the object';
+		if (this.mode == 'first') return 'an object';
+		if (this.mode == 'group') return 'the group of all objects'
+	}
+	var attrs = this.attrs.map(function (attr) { return attr.describe() }).join(" and ");
+	if (attrs != '') attrs = ' ' + attrs;
+	var rels = this.rels.map(function (rel) { return rel.describe() }).join(" and ");
+	if (this.mode == 'all') return 'all' + attrs + ' objects' + (rels == '' ? '' : ' that are ' + rels);
+	if (this.mode == 'unique') return 'the' + attrs + ' object' + (rels == '' ? '' : ' that is ' + rels);
+	if (this.mode == 'first') return 'a' + attrs + ' object' + (rels == '' ? '' : ' that is ' + rels);
+	if (this.mode == 'group') return 'the group of all objects that is' + attrs + (rels == '' ? '' : ' and ' + rels);
+};
+
+Selector.prototype.describe2 = function() {
+	if (this.empty()) {
+		if (this.mode == 'all') return 'all are objects';
+		if (this.mode == 'unique') return 'there is exactly one object';
+		if (this.mode == 'first') return 'this is an object';
+		if (this.mode == 'group') return '*'
+	}
+	var attrs = this.attrs.map(function (attr) { return attr.describe() });
+	var rels = this.rels.map(function (rel) { return rel.describe() });
+	var res = attrs.concat(rels).join(" and ");
+	if (this.mode == 'all') return 'all objects are ' + res;
+	if (this.mode == 'unique') return 'exactly one object is ' + res;
+	if (this.mode == 'first') return 'there is an object that is ' + res;
+	if (this.mode == 'group') return 'the group of objects is ' + res;
+};
+
+
+
+Selector.AttrMatcher = function(key, label, active, time) {
+	this.key = key;
+	this.label = label;
+	this.active = typeof(active) === 'undefined' ? true : active;
+	if (key in pbpSettings.obj_attrs) this.constant = pbpSettings.obj_attrs[key].prototype.constant;
+	else this.constant = pbpSettings.group_attrs[key].prototype.constant;
+	this.time = time || 'start';
+}
+
+/// Will extract the attribute's key, label, activation and constant property. Pass the time
+/// at which the attribute values should match (default: 'start').
+Selector.AttrMatcher.fromAttribute = function(attr, time) {
+	return new Selector.AttrMatcher(
+		attr.key, attr.get_label()
+	 ,attr.get_activity() >= pbpSettings.activation_threshold
+	 ,time);
+}
+
+/// Returns true if the passed node can supply the attribute and its activation and
+/// label match.
+Selector.AttrMatcher.prototype.matches = function(node) {
+	var attr = node.get(this.key, this.time);
+	if (!attr) return false;
+	console.log(this.key,'has activity',attr.get_activity());
+	var active = attr.get_activity() >= pbpSettings.activation_threshold;
+	return (active == this.active && attr.get_label() == this.label);
+}
+
+Selector.AttrMatcher.prototype.describe = function() {
+	return (this.active ? '' : 'not ') + this.label +
+				 (this.constant || this.time == "start" ? '' : ' at the ' + this.time);
+}
+
+/// CAUTION: other_sel is not allowed to use RelMatchers, itself! Otherwise
+/// we could get into infinite recursion!
+Selector.RelMatcher = function(other_sel, key, label, active, time) {
+	this.other_sel = other_sel;
+	this.key = key;
+	this.label = label;
+	this.active = typeof(active) === 'undefined' ? true : active;
+	this.constant = pbpSettings.obj_rels[key].prototype.constant;
+	this.symmetric = pbpSettings.obj_rels[key].prototype.symmetric;
+	this.time = time || 'start';
+}
+
+/// First uses its 'other' selector on the passed 'others' array of nodes. Returns true
+/// if the passed 'node' can supply the relationship to any of the selected nodes and
+/// the activation and label match.
+/// If others is not passed, all nodes in the scene except the 'node' are used.
+Selector.RelMatcher.prototype.matches = function(node, others) {
+	if (this.other_sel.rels.length > 0) throw "the other-selector of"
+	// select all other nodes in the scene as 'others', if they were not passed
+	others = others || node.scene_node.objs.filter(function (on) { return on !== node });
+	var res = this.other_sel.select(others, node.scene_node, (function (other) {
+		if (other === node) return false;
+		var rel = node.get(this.key, this.time, other);
+		if (!rel) return false;
+	  var active = rel.get_activity() >= pbpSettings.activation_threshold;
+		return (active == this.active && rel.get_label() == this.label);
+	}).bind(this));
+	return this.other_sel.mode == "all" ? res.length == others.length
+	                                    : res.length > 0;
+}
+
+/// Will extract the relation key, label, activation, constant and symmetry properties. Pass the time
+/// at which the attribute values should match (default: 'start'). Pass a selector that selects the other
+/// object.
+Selector.RelMatcher.fromRelationship = function(other, rel, time) {
+	return new Selector.RelMatcher(
+		other, rel.key, rel.get_label()
+	 ,rel.get_activity() >= pbpSettings.activation_threshold
+	 ,time);
+}
+
+Selector.RelMatcher.prototype.describe = function() {
+	return (this.active ? '' : 'not ') + this.label + " " +
+				 "[" + this.other_sel.describe() + "]" +
+				 (this.constant || this.time == "start" ? '' : ' at the ' + this.time);
+}
--- src/interpreter/solution.js
+++ src/interpreter/solution.js
@@ -2,6 +2,86 @@
 
 var Solution = {};
 
+/// Solution of the type "All/a/the object/group is X"
+/// main_side is either 'left' or 'right' (default: 'left').
+Solution.IsX = function(sel, main_side) {
+	this.sel = sel;
+	this.main_side = main_side || 'left';
+	this.other_side = {left: 'right', right: 'left'}[this.main_side];
+}
+
+/// Returns true if all scenes match the respecive selector.
+Solution.IsX.prototype.check = function(scenes_l, scenes_r) {
+	var a = this.main_side == 'left'  ? scenes_l : scenes_r
+	   ,b = this.main_side == 'right' ? scenes_l : scenes_r;
+	var sel = this.sel;
+	return a.every(function (scene) {
+			var objs = sel.select(scene.objs, scene);
+			var res = sel.mode == 'all' ? objs.length == scene.objs.length
+																	: objs.length > 0;
+			scene.fits_solution = res;
+			return res;
+	  }) && b.every(function (scene) {
+	  	var objs = sel.select(scene.objs, scene);
+	  	var res = sel.mode == 'all' ? objs.length < scene.objs.length
+	  														  : objs.length == 0;
+	  	scene.fits_solution = res;
+	  	return res;
+	  });
+};
+
+/// Returns a human readable description of the solution.
+Solution.IsX.prototype.describe = function() {
+	return "Only in the " + this.main_side + " scenes, " + this.sel.describe2();
+};
+
+
+
+
+/// Solution of the type "All/a/the object/group is X"
+/// main_side is either 'left' or 'right' (default: 'left').
+Solution.XIsY = function(sel1, sel2, main_side) {
+	this.sel1 = sel1;
+	this.sel2 = sel2;
+	this.main_side = main_side || 'left';
+	this.other_side = {left: 'right', right: 'left'}[this.main_side];
+}
+
+/// Returns true if all scenes match the respecive selector.
+Solution.XIsY.prototype.check = function(scenes_l, scenes_r) {
+	var a = this.main_side == 'left'  ? scenes_l : scenes_r
+	   ,b = this.main_side == 'right' ? scenes_l : scenes_r;
+	var sel1 = this.sel1, sel2 = this.sel2;
+	return a.every(function (scene) {
+			var objs = sel1.select(scene.objs, scene);
+			var okay = sel1.mode == 'all' ? objs.length == scene.objs.length
+							                     : objs.length > 0;
+			if (okay) {
+				var objs2 = sel2.select(objs, scene);
+				okay = sel2.mode == 'all' ? objs2.length == objs.length
+							                    : objs2.length > 0;
+			}
+			scene.fits_solution = okay;
+			return okay;
+	  }) && b.every(function (scene) {
+	  	var objs = sel1.select(scene.objs, scene);
+			var okay = sel1.mode == 'all' ? objs.length == scene.objs.length
+							                     : objs.length > 0;
+			if (okay) {
+				var objs2 = sel2.select(objs, scene);
+				okay = sel2.mode == 'all' ? objs2.length < objs.length
+							                    : objs2.length == 0;
+			}
+			scene.fits_solution = okay;
+			return okay;
+	  });
+};
+
+/// Returns a human readable description of the solution.
+Solution.XIsY.prototype.describe = function() {
+	return "Only in the " + this.main_side + " scenes, " + this.sel1.describe() + " are " + this.sel2.describe2();
+};
+
 /// Solution of the type "There is always an xxx on the main_side, but not on the other_side." ///////////////////////////
 /// main_side is either 'left' or 'right' (default: 'left').
 Solution.Exists = function(sel, main_side) {
@@ -15,39 +95,159 @@ Solution.Exists.prototype.check = function(scenes_l, scenes_r) {
 	var a = this.main_side == 'left'  ? scenes_l : scenes_r
 	   ,b = this.main_side == 'right' ? scenes_l : scenes_r;
 	var thiz = this;
-	return a.every(function (scene) { return thiz.sel.select(scene).length >= 1}) &&
-	    	 b.every(function (scene) { return thiz.sel.select(scene).length == 0});
+	return a.every(function (scene) { return thiz.sel.selectFirst(scene) }) &&
+	    	 b.every(function (scene) { return !thiz.sel.selectFirst(scene) });
 };
 
 /// Returns a human readable description of the solution.
 Solution.Exists.prototype.describe = function() {
-	return "There is an object that is " + this.sel.describe(true) +
+	return "There is an object that is " + this.sel.describe() +
 	       " in the " + this.main_side + " scenes, but not in the " +
 	       {left: 'right', right: 'left'}[this.main_side] + " scenes";
 };
 
 
-/// Solution of the type "Everything on the main_side is xxx, everything on the other_side is yyy." ///////////////////////////
+/// Solution of the type "On the main_side, every X is Y." ///////////////////////////
 /// main_side is either 'left' or 'right' (default: 'left').
-Solution.All = function(sel1, sel2, main_side) {
+Solution.All = function(sel, desc, main_side) {
 	this.main_side = main_side || 'left';
 	this.other_side = {left: 'right', right: 'left'}[this.main_side];
-	this.sel = {};
-	this.sel[this.main_side] = sel1;
-	this.sel[this.other_side] = sel2;
+	this.sel = sel || new ElementSelector();
+	this.desc = desc;
 }
 
 /// Returns true if all scenes match the respecive selector.
 Solution.All.prototype.check = function(scenes_l, scenes_r) {
 	var thiz = this;
-	return scenes_l.every(function (scene) { return scene.objs.every(thiz.sel.left.matches.bind(thiz.sel.left)) }) &&
-				 scenes_r.every(function (scene) { return !scene.objs.some(thiz.sel.left.matches.bind(thiz.sel.left)) }) &&
-				 scenes_r.every(function (scene) { return scene.objs.every(thiz.sel.right.matches.bind(thiz.sel.right)) }) &&
-				 scenes_l.every(function (scene) { return !scene.objs.some(thiz.sel.right.matches.bind(thiz.sel.right)) });
+	var f = function(sn, value) {
+		var ons = thiz.sel.selectAll(sn), N = ons.length;
+		return N != 0 && thiz.desc.matchesAll(ons) == value;
+	}
+	return scenes_l.every(function (scene) { return f(scene, thiz.main_side == 'left')  }) &&
+				 scenes_r.every(function (scene) { return f(scene, thiz.main_side == 'right') });
 };
 
 /// Returns a human readable description of the solution.
 Solution.All.prototype.describe = function() {
-	return "On the left every object is "  + this.sel.left.describe(true) +
-	     ", on the right every object is " + this.sel.right.describe(true);
+	return "On the "  +this.main_side+  " every " +
+	       (this.sel.empty() ? 'object' : "[" + this.sel.describe() + "]") +
+	       " is " + this.desc.describe();
 };
+
+
+/// Solution of the type "On the main_side, the X is R to the Y." ///////////////////////////
+/// main_side is either 'left' or 'right' (default: 'left').
+Solution.HasRelation = function(psel, main_side) {
+	this.main_side = main_side || 'left';
+	this.other_side = {left: 'right', right: 'left'}[this.main_side];
+	this.psel = psel;
+}
+
+/// Returns true if there is exactly one X, one Y and they are in relation R to
+/// each other.
+Solution.HasRelation.prototype.check = function(scenes_l, scenes_r) {
+	var thiz = this;
+	var for_left = this.main_side == 'left';
+	return scenes_l.every(function (scene) {
+				   return for_left == (thiz.psel.selectThisAndThis(scene)!=null)
+				 }) &&
+				 scenes_r.every(function (scene) {
+				   return for_left == (thiz.psel.selectThisAndThis(scene)==null)
+				 });
+};
+
+/// Returns a human readable description of the solution.
+Solution.HasRelation.prototype.describe = function() {
+	return "On the "  +this.main_side+  " the " + this.psel.describe();
+};
+
+
+/// Solution of the type "On the main_side, the X is Y." ///////////////////////////
+/// main_side is either 'left' or 'right' (default: 'left').
+Solution.HasAttribute = function(sel, desc, main_side) {
+	this.main_side = main_side || 'left';
+	this.other_side = {left: 'right', right: 'left'}[this.main_side];
+	this.sel = sel || new ElementSelector();
+	this.desc = desc;
+}
+
+/// Returns true if all scenes match the respecive selector.
+Solution.HasAttribute.prototype.check = function(scenes_l, scenes_r) {
+	var thiz = this;
+	var f = function(sn, value) {
+		var on = thiz.sel.selectThis(sn);
+		if (!on) return false;
+		return value == thiz.desc.matches(on);
+	}
+	return scenes_l.every(function (scene) { return f(scene, thiz.main_side == 'left')  }) &&
+				 scenes_r.every(function (scene) { return f(scene, thiz.main_side == 'right') });
+};
+
+/// Returns a human readable description of the solution.
+Solution.HasAttribute.prototype.describe = function() {
+	return "On the "  +this.main_side+  " the " +
+	       (this.sel.empty() ? 'object' : "[" + this.sel.describe() + "]") +
+	       " is " + this.desc.describe();
+};
+
+
+/// Solution of the type "On the main_side, the group of all objects is X." ///////////////////////////
+/// main_side is either 'left' or 'right' (default: 'left').
+Solution.SceneHasAttribute = function(desc, main_side) {
+	this.main_side = main_side || 'left';
+	this.other_side = {left: 'right', right: 'left'}[this.main_side];
+	this.desc = desc;
+}
+
+/// Returns true if all scenes match the respecive selector.
+Solution.SceneHasAttribute.prototype.check = function(scenes_l, scenes_r) {
+	var thiz = this;
+	var f = function(sn, value) {
+		var on = thiz.sel.selectThis(sn);
+		if (!on) return false;
+		return value == thiz.desc.matches(on);
+	}
+	return scenes_l.every(function (scene) { return f(scene, thiz.main_side == 'left')  }) &&
+				 scenes_r.every(function (scene) { return f(scene, thiz.main_side == 'right') });
+};
+
+/// Returns a human readable description of the solution.
+Solution.HasAttribute.prototype.describe = function() {
+	return "On the "  +this.main_side+  " the " +
+	       (this.sel.empty() ? 'object' : "[" + this.sel.describe() + "]") +
+	       " is " + this.desc.describe();
+};
+
+// /// Solution of the type "The xxx object is yyy on the main_side and zzz on the other_side." ///////////////////////////
+// /// main_side is either 'left' or 'right' (default: 'left').
+// Solution.X_is_A_vs_B = function(selx, sela, selb, main_side) {
+// 	this.main_side = main_side || 'left';
+// 	this.other_side = {left: 'right', right: 'left'}[this.main_side];
+// 	this.sel = {};
+// 	this.sel[this.main_side] = sela;
+// 	this.sel[this.other_side] = selb;
+// 	this.sel.x = selx;
+// }
+
+// /// Returns true if all scenes match the respecive selector.
+// Solution.X_is_A_vs_B.prototype.check = function(scenes_l, scenes_r) {
+// 	var thiz = this;
+// 	return (
+// 		scenes_l.every(function (scene) {
+// 			var xs = thiz.sel.x.select(scene);
+// 			return (xs.length == 1 &&  thiz.sel.left.matches(xs[0])
+// 				                     && !thiz.sel.right.matches(xs[0]));
+// 		}) &&
+// 		scenes_r.every(function (scene) {
+// 			var xs = thiz.sel.x.select(scene);
+// 			return (xs.length == 1 && !thiz.sel.left.matches(xs[0])
+// 				                     &&  thiz.sel.right.matches(xs[0]));
+// 		}));
+// };
+
+// /// Returns a human readable description of the solution.
+// Solution.X_is_A_vs_B.prototype.describe = function() {
+// 	return "The " + this.sel.x.describe(true) + " object is " +
+// 	       this.sel[this.main_side].describe(true)  + " in all " +this.main_side+ " scenes and " +
+// 	       this.sel[this.other_side].describe(true) + " in all " +this.other_side+ " scenes.";
+// };
