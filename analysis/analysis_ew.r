1db2002e9352c8809f93fd7ddb4a350457c9f924
Erik Weitnauer <eweitnauer@gmail.com>
2015-02-17 08:16:56 -0500

    new graphs for the cogsci2015 paper

--- analysis/analysis_ew.r
+++ analysis/analysis_ew.r
@@ -31,11 +31,16 @@ pbps = c('pbp02', 'pbp04', 'pbp08', 'pbp11b', 'pbp12', 'pbp13', 'pbp16', 'pbp18'
 data_ss4 = data_ss4[data_ss4$pbp %in% pbps,] # filter: only use pbps we also use in the machine learning case
 data_ss4$pbp = factor(data_ss4$pbp, levels=c(pbps)) # reorder factor levels
 data_ss4 = data_ss4[order(data_ss4$pbp),] # reorder columns according to the pbp factor
+data_ss4_a = data_ss4;
+data_ss4_a$flag = ' keep me';
+data_ss4_b = data_ss4;
+data_ss4_b$flag = 'delete me (I\'m identical)';
+data_ss4_ab = merge(data_ss4_a, data_ss4_b, all=T);
 
 data_all = merge(data_ai, data_ss[c("pbp", "cond", "found_solution", "train_time", "population")], all=T);
   
 # print solutions ordered by frequency per problem:
-pbp = 'pbp18';
+pbp = 'pbp36';
 sols = count(data[data$found_solution==1 & data$pbp==pbp,]$sol);
 sols[order(sols$freq),]
 
@@ -82,11 +87,16 @@ bargraph.CI(x.factor=pbp,group=sim_cond_bw_cat,response=subject_pairs_seen,data=
 
 ### all conds
 
-bargraph.CI(x.factor=pbp,group=cond,response=found_solution,ylim=c(0.0, 1.1),data=data,legend=T,ylab='correct answer rate')
+bargraph.CI(x.factor=pbp,group=cond,response=found_solution,ylim=c(0.0, 1.1),data=data,legend=F,ylab='correct answer rate')
 bargraph.CI(x.factor=pbp,group=cond,response=train_time,data=data,legend=T,ylab='steps to solution or fail')
 bargraph.CI(x.factor=pbp,group=cond,response=train_time,data=data[data$found_solution == 1,],legend=T,ylab='steps to solution for solved trials')
 
 bargraph.CI(x.factor=pbp,group=cond,response=found_solution,ylim=c(0.0, 1.1),data=data_ss,legend=T,ylab='correct answer rate')
+bargraph.CI(group=sim_cond_both_cat, x.factor=sch_cond, response=found_solution,ylim=c(0.0, 1.1),data=data_ss,legend=T,ylab='correct answer rate')
+bargraph.CI(x.factor=cond,response=found_solution,ylim=c(0.0, 1.1),data=data_ss,legend=T,ylab='correct answer rate')
+
+bargraph.CI(group=sim_cond_both_cat, x.factor=sch_cond, response=train_time,data=data_ai,legend=T,ylab='steps to solution or fail')
+bargraph.CI(group=sim_cond_both_cat, x.factor=sch_cond, response=train_time,data=data_ai[data_ai$found_solution == 1,],legend=T,ylab='steps for solved trials')
 
 ### feature1st
 
@@ -171,44 +181,122 @@ bargraph.CI(x.factor=sch_cond,response=found_solution,group=sim_cond_bw_cat,data
 
 
 ezANOVA(data=data,dv=found_solution,wid=mturk_id,within=.(sch_cond,sim_cond_wi_cat,sim_cond_bw_cat))
-ezANOVA(data=data,dv=train_time,wid=mturk_id,within=.(sch_cond,sim_cond_wi_cat,sim_cond_bw_cat))
-
 ezANOVA(data=data[data$found_solution==1,],dv=train_time,wid=mturk_id,within=.(sch_cond,sim_cond_wi_cat,sim_cond_bw_cat))
+
+ezANOVA(data=data,dv=train_time,wid=mturk_id,within=.(sch_cond,sim_cond_wi_cat,sim_cond_bw_cat))
+ezANOVA(data=data,dv=train_time,wid=mturk_id,within=.(sch_cond,sim_cond_wi_pair,sim_cond_bw_pair))
+
 
 ## graphs for the paper
 
 ### schedule, use?
 
-bargraph.CI(x.factor=sch_cond,response=train_time,data=data_ai[data_ai$found_solution == 1,],legend=T,ylab='train time')
-#bargraph.CI(x.factor=sch_cond,response=train_time,data=data_ai,legend=T,ylab='train time')
+#bargraph.CI(x.factor=sch_cond,response=train_time,data=data_ai[data_ai$found_solution == 1,],legend=T,ylab='train time')
+bargraph.CI(x.factor=sch_cond,response=train_time,data=data_ai,legend=T,ylab='train time')
 #bargraph.CI(x.factor=sch_cond,response=found_solution,data=data_ai,legend=T,ylab='train time', ylim=c(0.7,0.9))
 bargraph.CI(x.factor=sch_cond,response=found_solution,data=data_ss,legend=T,ylab='subjects acc')
 
 ### difficulty, use?
 
 bargraph.CI(x.factor=pbp,response=train_time,data=data_ai[data_ai$found_solution == 1,],legend=T,ylab='steps to solution for solved trials', main='AI')
-#bargraph.CI(x.factor=pbp,response=train_time,data=data_ai,legend=T,ylab='steps to solution or fail', main='AI')
-#bargraph.CI(x.factor=pbp,response=found_solution,ylim=c(0.0, 1.1),data=data_ai,legend=T,ylab='correct answer rate', main='AI')
-bargraph.CI(x.factor=pbp,response=found_solution,ylim=c(0.0, 1.1),data=data_ss,legend=T,ylab='correct answer rate', main='Subjects');
+bargraph.CI(x.factor=pbp,ylim=c(0,1.05),response=found_solution,data=data_ai,legend=T,ylab='correct answer rate'); #, main='AI')
+bargraph.CI(x.factor=pbp,ylim=c(0,2500),response=train_time,data=data_ai,legend=T,ylab='actions taken'); #, main='AI')
+bargraph.CI(x.factor=pbp,response=train_time,data=data_ai,legend=T,ylab='actions taken', main='AI')
+bargraph.CI(x.factor=pbp,response=1-found_solution,ylim=c(0.0, 1.1),data=data_ss,legend=T,ylab='no answer rate', main='Subjects');
 
 ### difficulty & schedule, don't use
 
-bargraph.CI(x.factor=pbp,group=sch_cond,response=train_time,data=data[data$found_solution == 1,],legend=T,ylab='steps to solution for solved trials')
-#bargraph.CI(x.factor=pbp,group=sch_cond,response=train_time,data=data,legend=T,ylab='steps to solution or fail')
+#bargraph.CI(x.factor=pbp,group=sch_cond,response=train_time,data=data[data$found_solution == 1,],legend=T,ylab='steps to solution for solved trials')
+bargraph.CI(x.factor=pbp,group=sch_cond,response=train_time,data=data,legend=T,ylab='steps to solution or fail')
+#bargraph.CI(x.factor=pbp,group=sch_cond,response=found_solution,data=data,legend=T,ylab='correct answer rate')
 bargraph.CI(x.factor=pbp,group=sch_cond,response=found_solution,ylim=c(0.0, 1.1),data=data_ss,legend=T,ylab='correct answer rate')
 
 ### all conditions, don't use
 
-bargraph.CI(x.factor=sch_cond,group=sim_cond_both_cat,response=train_time,data=data[data$found_solution==1,],legend=T,ylab='steps to solution for solved trials')
-bargraph.CI(x.factor=sch_cond,group=sim_cond_both_cat,response=found_solution,ylim=c(0.0, 1.1),data=data_ss,legend=T,ylab='correct answer rate')
+bargraph.CI(data=data[data$found_solution==1,],x.factor=sch_cond,group=sim_cond_both_cat,response=train_time,legend=T,ylab='steps to solution for solved trials')
+#bargraph.CI(x.factor=sch_cond,group=sim_cond_both_cat,response=found_solution,data=data,legend=T,ylab='steps to solution or fail')
+bargraph.CI(data=data_ss,x.factor=sch_cond,group=sim_cond_both_cat,response=found_solution,ylim=c(0.0, 1.1),legend=T,ylab='correct answer rate')
+bargraph.CI(data=data_ss[data_ss$found_solution==1,],x.factor=sch_cond,group=sim_cond_both_cat,response=subject_pairs_seen,legend=T,ylab='scene pairs seen in correct trials')
+bargraph.CI(data=data_ss[data_ss$found_solution==1,],x.factor=sch_cond,group=sim_cond_both_cat,response=train_time,legend=T,ylab='scene pairs seen in correct trials')
 
-### use? w/i sim. per schedule
+bargraph.CI(x.factor=sch_cond,group=sim_cond_both_cat,response=train_time,ylim=c(600,1200),data=data,legend=T,ylab='actions taken')
 
-bargraph.CI(x.factor=sch_cond, group=sim_cond_wi_cat,response=train_time,data=data[data$found_solution==1,],legend=T,ylab='steps to solution for solved trials', main='AI w/i similarity')
+### USE w/i sim. per schedule
+
+#bargraph.CI(x.factor=sch_cond, group=sim_cond_wi_cat,response=train_time,data=data[data$found_solution==1,],legend=T,ylab='steps to solution for solved trials', main='AI w/i similarity')
 #bargraph.CI(x.factor=sch_cond,response=found_solution,group=sim_cond_wi_cat,data=data_ss,legend=T,ylab='acc', main='Subjects w/i similarity')
-bargraph.CI(x.factor=sch_cond,response=found_solution,group=sim_cond_wi_cat,data=data_ss4,legend=T,ylab='acc', main='Subjects w/i similarity')
+
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="AI-wi-sim-by-sched.pdf",height=1.7, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+bargraph.CI(x.factor=sch_cond, group=sim_cond_wi_cat,response=train_time,data=data,legend=T,ylab='steps to solution or fail', ylim=c(600,1200)); #, main='AI w/i similarity'
+dev.off();
+
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="SS4-wi-sim-by-sched.pdf",height=1.7, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+#bargraph.CI(x.factor=sch_cond,response=1-found_solution,group=sim_cond_wi_cat,data=data_ss4,legend=T,ylab='wrong/no answer rate', ylim=c(0.2,0.8));# main='Subjects w/i similarity')
+bargraph.CI(x.factor=sim_cond_wi_cat,response=1-found_solution,data=data_ss4[data_ss4$sch_cond=='simultaneous',],legend=T,ylab='wrong/no answer rate', ylim=c(0.2,0.8));# main='Subjects w/i similarity')
+dev.off();
 
 ### USE b/w sim. per schedule
 
-bargraph.CI(x.factor=sch_cond, group=sim_cond_bw_cat,response=train_time,data=data[data$found_solution==1,],legend=T,ylab='steps to solution for solved trials', main='AI b/w similarity')
-bargraph.CI(x.factor=sch_cond,response=found_solution,group=sim_cond_bw_cat,data=data_ss,legend=T,ylab='acc', main='Subjects b/w similarity')
+#bargraph.CI(x.factor=sch_cond, group=sim_cond_bw_cat,response=train_time,data=data[data$found_solution==1,],legend=T,ylab='steps to solution for solved trials', main='AI b/w similarity')
+#bargraph.CI(x.factor=sch_cond, group=sim_cond_bw_cat,response=found_solution,data=data,legend=T,ylab='steps to solution or fail', main='AI b/w similarity', ylim=c(0.75,0.85))
+
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="AI-bw-sim-by-sched.pdf",height=1.7, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+bargraph.CI(x.factor=sch_cond, group=sim_cond_bw_cat,response=train_time,data=data,legend=T,ylab='steps to solution or fail', ylim=c(600,1200)); #, main='AI b/w similarity')
+dev.off();
+
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="SS3-bw-sim-by-sched.pdf",height=1.7, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+bargraph.CI(x.factor=sch_cond,response=1-found_solution,group=sim_cond_bw_cat,data=data_ss,legend=T,ylab='wrong/no answer rate', ylim=c(0.2,0.8));#, main='Subjects b/w similarity')
+dev.off();
+
+
+### FINAL PLOTS ###########################
+
+## w/i PAIR similarity for AI
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="AI-steps-by-wi-pair-sim.pdf",height=1.8, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+bargraph.CI(x.factor=sch_cond,response=train_time,group=sim_cond_wi_pair,data=data_ai,legend=T,ylab='action taken', ylim=c(600,1200), x.leg=3.6, y.leg=1200);
+dev.off();
+
+## w/i PAIR similarity for Ss3
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="SS3-error-by-wi-pair-sim.pdf",height=1.8, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+bargraph.CI(x.factor=sch_cond,response=1-found_solution,group=sim_cond_wi_pair,data=data_ss,legend=T,ylab='error rate', ylim=c(0,1.01), x.leg=3.6);
+dev.off();
+
+## w/i category similarity for Ss4
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="SS4-error-by-wi-cat-sim.pdf",height=1.8, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+bargraph.CI(group=sim_cond_wi_cat,x.factor=flag,response=1-found_solution,data=data_ss4_ab,legend=T,ylab='error rate', ylim=c(0,1.01), x.leg=1);
+dev.off();
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="SS4-error-by-bw-cat-sim.pdf",height=1.8, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+bargraph.CI(x.factor=flag, group=sim_cond_bw_cat,response=1-found_solution,data=data_ss4_ab,legend=T,ylab='error rate', ylim=c(0,1.01), x.leg=0.85);
+dev.off();
+
+
+## AI performance per problem
+
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="AI-acc-per-problem.pdf",height=1.0, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+bargraph.CI(x.factor=pbp,ylim=c(0,1.05),response=found_solution,data=data_ai,legend=T,ylab='correct answer rate'); #, main='AI')
+dev.off();
+
+setwd("~/Code/diss/modelling/current/analysis")
+pdf(file="AI-steps-per-problem.pdf",height=1.0, width=3.4, pointsize=7)
+par(mar=c(2,4,1,0)+0.2)
+bargraph.CI(x.factor=pbp,ylim=c(0,2500),response=train_time,data=data_ai,legend=T,ylab='actions taken'); #, main='AI')
+dev.off();
+
--- analysis/loading.r
+++ analysis/loading.r
@@ -3,13 +3,15 @@
 # renames the following columns: mturk_id (rep), found_solution (solved), cond (pres_mode), train_time (steps)
 load_data = function() {
   #data = read.csv("pbp-computer-results.csv", header=TRUE, colClasses=c("steps"="numeric"));
-  data = read.csv("data-0-5-5.csv", header=TRUE, colClasses=c("steps"="numeric"));
+  data = read.csv("data-0-5-6-latest.csv", header=TRUE, colClasses=c("steps"="numeric"));
   data = rename(data, c("solved"="found_solution","pres_mode"="cond","steps"="train_time","rep"="mturk_id"));
   data$found_solution = ifelse((data$found_solution == 'true') | (data$found_solution == 1), 1, 0);
   
   # for now, don't use problems PBP35 and PBP36
   pbps = c('pbp02', 'pbp04', 'pbp08', 'pbp11b', 'pbp12', 'pbp13', 'pbp16', 'pbp18', 'pbp20', 'pbp22', 'pbp26', 'pbp31');#, 'pbp35', 'pbp36');
   data = data[data$pbp %in% pbps,]
+  data$pbp = revalue(data$pbp, c("pbp02"="2", "pbp04"="4", "pbp08"="8", "pbp11b"="11b", 'pbp12'='12', 'pbp13'='13', 'pbp16'='16', 'pbp18'='18', 'pbp20'='20', 'pbp22'='22', 'pbp26'='26', 'pbp31'='31'));#, 'pbp35', 'pbp36');))
+  data = droplevels(data)
   
   print("subjects total:")
   print(length(table(data$mturk_id)))
@@ -27,12 +29,19 @@ annotate_data = function(data) {
   #create new conditions, to separate each factor (sim and schedule)
   data$sch_cond = ifelse(data$cond %in% c('blocked-dis-dis','blocked-sim-sim','blocked-dis-sim','blocked-sim-dis'),'blocked','interleaved')
   
-  # are the comparisons within one category done on similar or dissimilar elements?
-  data$sim_cond_wi_cat  = ifelse(data$cond %in% c('blocked-dis-sim','interleaved-dis-sim','interleaved-dis-dis','blocked-dis-dis'),'dissimilar','similar')
-  data$sim_cond_bw_cat = ifelse(data$cond %in% c('blocked-dis-sim','interleaved-dis-sim','interleaved-sim-sim','blocked-sim-sim'),'similar','dissimilar')
+  # similarity within scene pairs and between scene pairs
+  data$sim_cond_wi_pair = ifelse(data$cond %in% c('blocked-dis-sim','interleaved-dis-sim','interleaved-dis-dis','blocked-dis-dis'),'dissimilar scenes paired','similar scenes paired')
+  data$sim_cond_bw_pair = ifelse(data$cond %in% c('blocked-dis-sim','interleaved-dis-sim','interleaved-sim-sim','blocked-sim-sim'),'similar pairs next to each other','dissimilar pairs next to each other')
+  
+  # similarity in within category and between category comparisions
+  #   since - same as in experiment 3.2 - the original similarity conditions were named based on w/i and b/w *pair* similarity,
+  #   we need to swap interleaved-dis-sim with interleaved-sim-dis to get to the w/i and b/w *category* similarities we want.
+  data$sim_cond_wi_cat = ifelse(data$cond %in% c('blocked-dis-sim','blocked-dis-dis', 'interleaved-dis-dis','interleaved-sim-dis'), 'dis. w/i cat.','sim. w/i cat.')
+  data$sim_cond_bw_cat = ifelse(data$cond %in% c('blocked-dis-dis','blocked-sim-dis', 'interleaved-dis-dis','interleaved-dis-sim'), 'dis. b/w cat.','sim. b/w cat.')
+  
   
   # this line is *much* faster than the loop below
-  data$sim_cond_both_cat = interaction(data$sim_cond_wi_cat, data$sim_cond_bw_cat);
+  data$sim_cond_both_cat = interaction(data$sim_cond_wi_cat, data$sim_cond_bw_cat, sep=', ');
   # create new condition sim_cond_both, which can take one of four values
   #for(i in 1:length(data$cond)) {
   #  cond_str = unlist(strsplit(as.character(data$cond[i]), '-'));
@@ -42,7 +51,7 @@ annotate_data = function(data) {
   #}
   
   # reorder similarity factor levels
-  #data$sim_cond_both_cat = factor(data$sim_cond_both_cat, c("dis-dis", "sim-dis", "dis-sim", "sim-sim"));
+  #data$sim_cond_both_cat = factor(data$sim_cond_both_cat, c("dis. w/i cat., dis. b/w cat.", "sim. w/i cat., dis. b/w cat.", "dis. w/i cat., sim. b/w cat.", "sim. w/i cat., sim. b/w cat."));
   
   # new idea by Rob: use the problem difficulty as a covariate!
   # calculate problem difficulty as average accuracy
